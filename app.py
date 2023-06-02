c6ae9caa1451a11326ad288f6f55eed290c4951d
datsiii <dasha.tsivina@gmail.com>
2023-06-02 08:20:58 +0300

    fixes

--- app.py
+++ app.py
@@ -43,16 +43,17 @@ def get_index():
     if 'index' not in g:
         g.index = create_index()
 
-    return g.db
+    return g.index
 
-def create_index(labels):
+def create_index():
     index = dict()
     with open("sample.json", "r") as infile:
-        result = json.load(infile)
+        labels = json.load(infile)
         for landmark, value in labels.items():
             for categ, probab in value.items():
                 if categ not in index or probab > index[categ][1]:
                     index[categ] = (landmark, probab)
+    print("index size: ", len(index))
     return index
 
 
@@ -65,8 +66,9 @@ def handle_request():
 def get_file():
     base64_img = request.form["image"]
 
+    image_path = 'download/decoded_image.jpeg'
     # Get image from POST request
-    with open('decoded_image.jpeg', 'wb') as file_to_save:
+    with open(image_path, 'wb') as file_to_save:
         # decoded_image_data = base64.decodebytes(base64_img_bytes)
         decoded_image_data = urllib.parse.unquote(base64_img)
         base64_img_bytes = decoded_image_data.encode('utf-8')
@@ -74,14 +76,14 @@ def get_file():
         file_to_save.write(decoded_image_data)
 
     # get image labels via ResNet
-    img_path = os.path.join("path")
-    img = image.load_img(img_path, target_size=(224, 224))
+    img = image.load_img(image_path, target_size=(224, 224))
     # превращаем фото в numpy массив для того чтобы подать на вход нейросети
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
     x = preprocess_input(x)
     preds = model.predict(x)
     _, category, probability = decode_predictions(preds, top=1)[0][0]
+    print("category", category)
     probability = float(probability)
     # preproces_image
     # preds = model.predict(x)
@@ -89,16 +91,14 @@ def get_file():
 
     # find matching landmark
     index = get_index()
-    labels = create_index(index)
     # use index to search landmarks
     #answer = create_index(index)
-    with open("sample.json", "r") as infile:
-        result = json.load(infile)
-        for landmark, value in labels.items():
-            for categ, probab in value.items():
-                if category == categ:
-                    response = "landmark: " + str(landmark)
-                    return response
+    if category in index:
+        print("recognized: ", index[category][0])
+        return index[category][0]
+    else:
+        print("not recognized")
+        return "not recognized"
 
     '''
     base64_bytes = base64_message.encode('ascii')
